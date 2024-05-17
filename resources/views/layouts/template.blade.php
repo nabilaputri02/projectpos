40c8811f63b2d63c285ab6af38ca0241086d2fb8
nabilaputri02 <np021206@gmail.com>
2024-05-16 21:23:56 -0700

    merubah template.blade

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -72,7 +72,7 @@
             <!-- Brand Logo -->
             <a href="index3.html" class="brand-link">
                 <img src="{{ asset('dist/img/toko.jpg')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">Toko Buah</span>
+                <span class="brand-text font-weight-light">TOKO BUAH</span>
             </a>
 
             <!-- Sidebar -->
